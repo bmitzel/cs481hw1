b228253e17cdb7d962fb221e8ceed5b00957cbb9
Brian Mitzel <bmitzel@users.noreply.github.com>
2014-10-05 10:52:45 -0700

    Improved input routines
    Improved all of the inputs at the start of the program to make them
    cleaner and more robust. This fixes any crashing issues.

--- AIChessGame.py
+++ AIChessGame.py
@@ -13,6 +13,19 @@ class AIChessGame(object):
 	def printError(self):
 		print("Invalid input. Try again.\n")
 
+	def getXY(self, prompt):
+		try:
+			value = int(input(prompt))
+		except ValueError:
+			value = 0
+		while (value < 1 or value > 8):
+			self.printError()
+			try:
+				value = int(input(prompt))
+			except ValueError:
+				pass
+		return value
+
 	def start(self):
 		# Print welcome message
 		print("Starting AIChessGame...\n")
@@ -20,50 +33,40 @@ class AIChessGame(object):
 		print("Valid values for X and Y are from 1-8.")
 		
 		# Get the starting positions for all 3 pieces: White King, White Rook, Black King
-		wkX = int(input("White King X: "))
-		while (wkX < 1 or wkX > 8):
-			self.printError()
-			wkX = int(input("White King X: "))
-		wkY = int(input("White King Y: "))
-		while (wkY < 1 or wkY > 8):
-			self.printError()
-			wkY = int(input("White King Y: "))
-		wrX = int(input("White Rook X: "))
-		while (wrX < 1 or wrX > 8):
-			self.printError()
-			wrX = int(input("White Rook X: "))
-		wrY = int(input("White Rook Y: "))
-		while (wrY < 1 or wrY > 8):
-			self.printError()
-			wrY = int(input("White Rook Y: "))
-		bkX = int(input("Black King X: "))
-		while (bkX < 1 or bkX > 8):
-			self.printError()
-			bkX = int(input("Black King X: "))
-		bkY = int(input("Black King Y: "))
-		while (bkY < 1 or bkY > 8):
-			self.printError()
-			bkY = int(input("Black King Y: "))
-		self.players = [WhitePlayer(Position(wkX, wkY), Position(wrX, wrY)),
-			BlackPlayer(Position(bkX, bkY))]
-		self.board = Board(self.players[0], self.players[1])
-		
+		wkX = self.getXY("White King X: ")
+		wkY = self.getXY("White King Y: ")
+		wrX = self.getXY("White Rook X: ")
+		wrY = self.getXY("White Rook Y: ")
+		bkX = self.getXY("Black King X: ")
+		bkY = self.getXY("Black King Y: ")
+
 		# Get whether or not to use heuristicY
-		useHeuristicY = input("Use heuristicY for the Black player (Y/N)? ").upper()
+		useHeuristicY = input("Use heuristicY for the Black player (Y/N)? ").upper()[:1]
 		while (useHeuristicY.upper() != "Y" and useHeuristicY.upper() != "N"):
 			self.printError()
-			useHeuristicY = input("Use heuristicY for the Black player (Y/N)? ").upper()
+			useHeuristicY = input("Use heuristicY for the Black player (Y/N)? ").upper()[:1]
 		if useHeuristicY == "Y":
 			self.useHeuristicY = True
 		else:
 			self.useHeuristicY = False
 
 		# Get the max number of moves for each player
-		self.n = int(input("Enter the max number of moves per player: "))
+		try:
+			self.n = int(input("Enter the max number of moves per player: "))
+		except ValueError:
+			self.n = 0
 		while (self.n < 1):
 			self.printError()
-			self.n = int(input("Enter the max number of moves per player: "))
+			try:
+				self.n = int(input("Enter the max number of moves per player: "))
+			except ValueError:
+				pass
 
+		# Create and initialize the players and the chess board
+		self.players = [WhitePlayer(Position(wkX, wkY), Position(wrX, wrY)),
+			BlackPlayer(Position(bkX, bkY))]
+		self.board = Board(self.players[0], self.players[1])
+		
 		# DEBUG: Print the players and their pieces
 		print("\n" + str(self.players[0]) + " " + str(self.players[0].pieces[0]) + " at " + str(self.players[0].pieces[0].position))
 		print(str(self.players[0]) + " " + str(self.players[0].pieces[1]) + " at " + str(self.players[0].pieces[1].position))
@@ -239,37 +242,6 @@ class Board(object):
 		print("   +----+----+----+----+----+----+----+----+")
 		print("     1    2    3    4    5    6    7    8")
 		
-		# 0-10, squares 0 and 9 are used for underAttack calculations. 
-		# Board is limited 1-8
-
-		# self.board = [[(x,y) for x in xrange(1,9)] for y in xrange(1,9)]
-		# self.white = white
-		# self.black = black
-		# black.rook = Piece('r', (8,5), black)
-		# black.king = Piece('k', (6,8), black)
-		# white.king = Piece('k', (5,6), white)
-
-		#8 = 	56 57 58 59 60 61 62 63  |8
-		#7 = 	48 49 50 51 52 53 55 55  |7
-		#6 = 	40 41 42 43 44 45 46 47  |6
-		#5 = 	32 33 34 35 36 37 38 39  |5
-		#4 = 	24 25 26 27 28 29 30 31  |4
-		#3 = 	16 17 18 19 20 21 22 23  |3
-		#2 = 	8  9  10 11 12 13 14 15  |2
-		#1 = 	0  1   2  3  4  5  6  7  |1 
-		#line	--------------------------
-		#0 =    A   B  C  D  E  F  G  H  0
-
-	# def boardState(self):
-	# 	print self.board[7]
-	# 	print self.board[6]
-	# 	print self.board[5]
-	# 	print self.board[4]
-	# 	print self.board[3]
-	# 	print self.board[2]
-	# 	print self.board[1]
-	# 	print self.board[0]
-
 class Play(object):
 	def __init__(self, black, white):
 		#self.board = dict()
