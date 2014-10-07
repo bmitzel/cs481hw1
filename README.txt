ad247e129ace74560a329627f8d86fe3cdb8c83c
Brian Mitzel <bmitzel@users.noreply.github.com>
2014-10-07 13:10:47 -0700

    Add random movement for both players
    Added random movement for both White and Black players. For White, this
    is for debugging purposes only. White's random movement functionality
    can be enabled or disabled with the global variable debugWhiteRandom.
    
    Added updating of each player's list of pieces after each move.
    
    Stubbed out the Player base class because we may not need it.
    
    Added several comments to improve program readability.
    
    Updated the README accordingly.

--- README.txt
+++ README.txt
@@ -1,8 +1,6 @@
 10-6
-move players randomly check with Brick
 checkmate, stalemate
 fix routine ( add outside coordinates)
-update list of pieces, beginning, clear list, grab new list
 when game ends, result # of moves
 
 
--- play.py
+++ play.py
@@ -29,12 +29,12 @@ def play(game):
 	printStart(game.players, game.useHeuristicY)
 	game.printBoard()
 	# Disabling n for now and just using 1 move per player
-	#for n in range(game.n):
-	for n in range(1):
+	for n in range(game.n):
+	#for n in range(1):
 		for player in game.players:
 			# player.move() (this function both makes and prints the move e.g. "Rook to (1, 1)")
 			player.move(game)
-			# game.printBoard()
+			game.printBoard()
 			# check for stale mate and check mate
 			#   if so, print the result
 			#   return
